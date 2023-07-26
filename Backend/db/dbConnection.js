81837cc3996aee3f9dd5ecb97c789a00fee6d8d6
OmarFawzy74 <106036912+OmarFawzy74@users.noreply.github.com>
2023-07-26 04:08:54 +0300

    last commit

--- Backend/db/dbConnection.js
+++ Backend/db/dbConnection.js
@@ -5,8 +5,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    database: process.env.DB_NAME
 });
 
 //  host: "localhost",
