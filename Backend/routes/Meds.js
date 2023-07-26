06973c57298a157d95d811372465fff5c93f77db
OmarFawzy74 <106036912+OmarFawzy74@users.noreply.github.com>
2023-07-26 22:45:25 +0300

    a commit

--- Backend/routes/Meds.js
+++ Backend/routes/Meds.js
@@ -162,7 +162,8 @@ router.delete("",
             }
 
             //2- REMOVE Old Medicine IMAGE
-            fs.unlinkSync(__dirname + "/upload/" + medicine[0].image_url);
+            // fs.unlinkSync(__dirname + "/upload/" + medicine[0].image_url);
+            fs.unlinkSync("https://sangria-python-toga.cyclic.app/"+ medicine[0].image_url);
 
             await query("delete from medicines where id = ?", [medicine[0].id]);
             
